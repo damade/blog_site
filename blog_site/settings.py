54c4f435b81e96ce3e2db30d3140ca77da4d4661
damade <damilolaadeoye545@yahoo.com>
2020-08-12 18:07:03 +0100

    feat: profile page

--- blog_site/settings.py
+++ blog_site/settings.py
@@ -120,7 +120,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'blog-home'
+
+LOGIN_URL = 'login'
--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,9 @@
 from django import forms
-from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+from .models import Profile
+
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
@@ -10,3 +13,41 @@ class UserRegisterForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username','email','firstName','lastName','password1','password2']
+
+    def save(self, commit=True):
+        user = super(UserRegisterForm, self).save(commit=False)
+        firstName = self.cleaned_data["firstName"]
+        lastName = self.cleaned_data["lastName"]
+        user.first_name = firstName
+        user.last_name = lastName
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+    firstName = forms.CharField(max_length=50, required=True, label="First Name")
+    lastName = forms.CharField(max_length=50, required=True, label="Last Name")
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'firstName', 'lastName']
+
+    def save(self, commit=True):
+        user = super(UserUpdateForm, self).save(commit=False)
+        firstName = self.cleaned_data["firstName"]
+        lastName = self.cleaned_data["lastName"]
+        user.first_name = firstName
+        user.last_name = lastName
+        user.email = self.cleaned_data["email"]
+        if commit:
+            user.save()
+        return user
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image', 'bio']
